3ea04a6e3b8b377f416d6759caa5fe76ae19ee2a
Dave Carruthers <dave@dweb-x.com>
2016-03-07 13:50:02 +0000

    Added messages header to all projects

---
+++ projects/convTest/include/messages.h
@@ -0,0 +1,39 @@
+#ifndef MESSAGES_H
+#define MESSAGES_H
+#include <stdint.h>
+//Might need to change these ID's at some point
+#define CONVEYOR_ID 3
+#define INPUT_ROBOT_ID 6
+#define OUTPUT_ROBOT_ID 9
+#define CONTROLLER_ID 12
+
+//All CAN messages
+typedef emum mType{
+    START,
+    ERROR,
+    PAUSE,
+    STOP,
+    INPUT_READY,
+    OUTPUT_READY,
+    CONVEYOR_READY,
+    INPUTPAD_LOADED,
+    PICKUP_ATTEMPTED,
+    PICKUP_OK,
+    PICKUP_FAILED,
+    REQ_PLACEBLOCK,
+    ACK_PLACEBLOCK,
+    REQ_REMOVEBLOCK,
+    ACK_REMOVEBLOCK,
+    REMOVE_BLOCK,
+    REMOVE_ATTEMPTED,
+    REMOVE_FAILED,
+    REMOVE_OK
+} messageType;
+
+//Message consists of message type plus optional data.
+typedef struct{
+    messageType type,
+    uint16_t mData;
+} message;
+
+#endif // MESSAGES_H_INCLUDED
