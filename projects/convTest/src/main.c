6f0987a0a21c2700b33631876c87b0f775d6dd5b
jonathan greenlees <jonathan.greenlees@hotmail.co.uk>
2016-05-03 12:09:00 +0000

    main.c edited. task update

--- projects/convTest/src/main.c
+++ projects/convTest/src/main.c
@@ -18,14 +18,21 @@
 #include <buttons.h>
 #include <interface.h>
 #include <conveyor.h>
+#include "messages.h"
+#include <can.h>
+#include <lcd.h>
+#include <interface.h>
+#include <timers.h>
 
 /*************************************************************************
 *                  PRIORITIES
 *************************************************************************/
 
 enum {
-  APP_TASK_MONITOR_SENS_PRIO = 4,
-  APP_TASK_CTRL_CONV_PRIO
+    APP_TASK_SENSOR_TWO_PRIO= 4,
+    APP_TASK_SENSOR_ONE_PRIO,
+    APP_TASK_BLOCK_PLACED_PRI0 ,
+    APP_TASK_BLOCK_REMOVED_PRI0,
 };
 
 /*************************************************************************
@@ -33,130 +40,246 @@ enum {
 *************************************************************************/
 
 enum {
-  APP_TASK_MONITOR_SENS_STK_SIZE = 256,
-  APP_TASK_CTRL_CONV_STK_SIZE = 256
+  APP_TASK_SENSOR_TWO_SIZE = 256,
+  APP_TASK_BLOCK_PLACED_SIZE = 256,
+  APP_TASK_BLOCK_REMOVED_SIZE = 256,
+  APP_TASK_SENSOR_ONE_SIZE = 256
 };
 
-static OS_STK appTaskMonitorSensStk[APP_TASK_MONITOR_SENS_STK_SIZE];
-static OS_STK appTaskCtrlConvStk[APP_TASK_CTRL_CONV_STK_SIZE];
+static OS_STK appTaskSensorTwoStk[APP_TASK_SENSOR_TWO_SIZE];
+static OS_STK appTaskBlockPlacedStk[APP_TASK_BLOCK_PLACED_SIZE];
+static OS_STK appTaskBlockRemovedStk[APP_TASK_BLOCK_REMOVED_SIZE];
+static OS_STK appTaskSensorOneStk[APP_TASK_SENSOR_ONE_SIZE];
+
 
 /*************************************************************************
 *                  APPLICATION FUNCTION PROTOTYPES
 *************************************************************************/
 
-static void appTaskMonitorSens(void *pdata);
-static void appTaskCtrlConv(void *pdata);
+static void appTaskSensorTwo(void *pdata);
+static void appTaskBlockPlaced(void *pdata);
+static void appTaskBlockRemoved(void *pdata);
+static void appTaskSensorOne(void *pdata);
 
+
+/*************************************************************************
+*                  GLOBAL VARIABLES
+*************************************************************************/
+int count = 0;
+bool arr;
+uint32_t timeElapsed = 0;
 /*************************************************************************
 *                    GLOBAL FUNCTION DEFINITIONS
 *************************************************************************/
+static bool startupChecks(void);
 
 int main() {
   /* Initialise the hardware */
   bspInit();
-  conveyorInit();
- 
-  /* Initialise the OS */
-  OSInit();                                                   
+  conveyorInit(); 
+  OSInit();               
+  initWatch() ;
 
   /* Create Tasks */
-  OSTaskCreate(appTaskMonitorSens,                               
+   OSTaskCreate(appTaskBlockPlaced,                               
                (void *)0,
-               (OS_STK *)&appTaskMonitorSensStk[APP_TASK_MONITOR_SENS_STK_SIZE - 1],
-               APP_TASK_MONITOR_SENS_PRIO);
+               (OS_STK *)&appTaskBlockPlacedStk[APP_TASK_BLOCK_PLACED_SIZE - 1],
+               APP_TASK_BLOCK_PLACED_PRI0);
+     
+//   OSTaskCreate(appTaskBlockRemoved,                               
+//               (void *)0,
+//               (OS_STK *)&appTaskBlockRemovedStk[APP_TASK_BLOCK_REMOVED_SIZE - 1],
+//                APP_TASK_BLOCK_REMOVED_PRI0);
    
-  OSTaskCreate(appTaskCtrlConv,                               
+    OSTaskCreate(appTaskSensorTwo,                               
                (void *)0,
-               (OS_STK *)&appTaskCtrlConvStk[APP_TASK_CTRL_CONV_STK_SIZE - 1],
-               APP_TASK_CTRL_CONV_PRIO);
-   
-  /* Start the OS */
-  OSStart();                                                  
+               (OS_STK *)&appTaskSensorTwoStk[APP_TASK_SENSOR_TWO_SIZE - 1],
+               APP_TASK_SENSOR_TWO_PRIO);
   
+    OSTaskCreate(appTaskSensorOne,                               
+               (void *)0,
+               (OS_STK *)&appTaskSensorOneStk[APP_TASK_SENSOR_ONE_SIZE - 1],
+               APP_TASK_SENSOR_ONE_PRIO);
+   
+     
+    if(startupChecks) {    
+      OSStart();             
+    }
+  /* Start the OS */                                              
   /* Should never arrive here */ 
   return 0;      
 }
 
+static bool startupChecks(void)
+{
+//  bool ready;
+//  canMessage_t txMsg;
+//  canMessage_t rxMsg;
+//  while(!ready){
+//    if (canReady(CAN_PORT_1)){
+//      canRead(CAN_PORT_1, &rxMsg);
+//      if (rxMsg.dataA==STARTUP){
+//        txMsg.dataA = CONVEYOR_READY;
+//        canWrite(CAN_PORT_1, &txMsg);
+//      }
+//    }
+//  }
+  return true;
+}
 /*************************************************************************
 *                   APPLICATION TASK DEFINITIONS
 *************************************************************************/
 
-static void appTaskMonitorSens(void *pdata) {
-    
-  /* Start the OS ticker
-   * (must be done in the highest priority task)
-   */
-  osStartTick();
+static void appTaskSensorTwo(void *pdata) {
   
-  /* 
-   * Now execute the main task loop for this task
-   */
+bool startCounter ;
+
+  osStartTick();
   while (true) {
-    interfaceLedSetState(D1_LED | D2_LED | D3_LED | D4_LED, LED_OFF);
-    ledSetState(USB_LINK_LED, LED_OFF);
-    ledSetState(USB_CONNECT_LED, LED_OFF);
-    
-    if (conveyorItemPresent(CONVEYOR_SENSOR_1)) {
-        interfaceLedSetState(D1_LED, LED_ON);
-        ledSetState(USB_LINK_LED, LED_ON);
+    if (conveyorItemPresent(CONVEYOR_SENSOR_2) &&(conveyorItemPresent(CONVEYOR_SENSOR_1) ))
+    {
+      
+           conveyorSetState(CONVEYOR_OFF);
+    }
+    else
+    {      
+       if (conveyorItemPresent(CONVEYOR_SENSOR_2) )
+    {        
+            
+       if (!startCounter)
+    {
+       count++;
+       startCounter = true;
+    }
+      
+      OSTimeDly(2000);
+      conveyorSetState(CONVEYOR_REVERSE);   
     } 
-    if (conveyorItemPresent(CONVEYOR_SENSOR_2)) {
-        interfaceLedSetState(D2_LED, LED_ON);
-        ledSetState(USB_CONNECT_LED, LED_ON);
+    else
+    {
+      startCounter = false;
+    }     
+
+    if (count > 0 && !conveyorItemPresent(CONVEYOR_SENSOR_1))
+    {
+        conveyorSetState(CONVEYOR_REVERSE); 
+     
+    }    
+    }
+      OSTimeDly(20);
+
+  } 
+}
+
+static void appTaskSensorOne(void *pdata) {
+  bool startCounter;  
+  canMessage_t txMsg;
+
+    
+    while(true){  
+    if (conveyorItemPresent(CONVEYOR_SENSOR_2) &&(conveyorItemPresent(CONVEYOR_SENSOR_1) ))
+    {
+           conveyorSetState(CONVEYOR_OFF);
+    }
+    
+    else
+    {
+       if (conveyorItemPresent(CONVEYOR_SENSOR_1) ) 
+    { 
+      
+      if (!startCounter)
+      {
+            count --;      
+            startCounter = true;
+            if(count < 0)
+              count = 0;      
+       
+                 txMsg.id = INPUT_ROBOT_ID;
+                 txMsg.dataA = ACK_PLACEBLOCK;
+                 txMsg.len = 4;
+                 canWrite(CAN_PORT_1, &txMsg);         
+      }
+
+      OSTimeDly(200);
+      conveyorSetState(CONVEYOR_OFF); 
+      
+//    timeElapsed = stopWatch () ;//*********************
+//    lcdSetTextPos(2,1) ;
+//    lcdWrite("%010d " , timeElapsed ) ;
     } 
     
+    else{
+       startCounter = false;
+    }
+   
+    if(count <= 0)
+    {
+      conveyorSetState(CONVEYOR_OFF); 
+    }
+   }     
     OSTimeDly(20);
   }
 }
 
-static void appTaskCtrlConv(void *pdata) {
-  uint32_t btnState;
-  
-  while (true) {
-    static bool jsLRPressed = false;
-    static bool jsUpPressed = false;
-    static bool jsDownPressed = false;
-    btnState = buttonsRead();
-
-    if (isButtonPressedInState(btnState, JS_LEFT) || 
-        isButtonPressedInState(btnState, JS_RIGHT)) {
-      jsLRPressed = true;
+static void appTaskBlockRemoved(void *pdata) {    
+    canMessage_t txMsg;
+    canMessage_t rxMsg;
+    while(true)
+    {        
+            if (conveyorItemPresent(CONVEYOR_SENSOR_1))
+              {
+                txMsg.id = OUTPUT_ROBOT_ID;
+                txMsg.dataA = REMOVE_BLOCK;
+                txMsg.len = 4;
+                canWrite(CAN_PORT_1, &txMsg);
+              
+                if (canReady(CAN_PORT_1)){
+                    canRead(CAN_PORT_1, &rxMsg);
+                    
+                    if(rxMsg.dataA==REMOVE_ATTEMPTED){
+                      if(!conveyorItemPresent(CONVEYOR_SENSOR_1)){                      
+                       txMsg.id = OUTPUT_ROBOT_ID;
+                       txMsg.dataA = REMOVE_OK;
+                       txMsg.len = 4;
+                       canWrite(CAN_PORT_1, &txMsg);
+                       }
+                      
+                      else{  
+                       txMsg.id = OUTPUT_ROBOT_ID;
+                       txMsg.dataA = REMOVE_FAILED;
+                       txMsg.len = 4;
+                       canWrite(CAN_PORT_1, &txMsg);
+                       }
+                    }                
+                }
+              }
+    OSTimeDly(20);    
     }
-    if (jsLRPressed && (!isButtonPressedInState(btnState, JS_LEFT))
-                    && (!isButtonPressedInState(btnState, JS_RIGHT))) {
-      jsLRPressed = false;
-      conveyorSetState(CONVEYOR_OFF);
-    }
-
-    if (isButtonPressedInState(btnState, JS_UP)) {
-      jsUpPressed = true;
-    }
-    if (jsUpPressed && (!isButtonPressedInState(btnState, JS_UP))) {
-      jsUpPressed = false;
-      conveyorSetState(CONVEYOR_OFF);
-      OSTimeDly(250);
-      conveyorSetState(CONVEYOR_FORWARD);
-    }
-    
-    if (isButtonPressedInState(btnState, JS_DOWN)) {
-      jsDownPressed = true;
-    }
-    if (jsDownPressed && (!isButtonPressedInState(btnState, JS_DOWN))) {
-      jsDownPressed = false;
-      conveyorSetState(CONVEYOR_OFF);
-      OSTimeDly(250);
-      conveyorSetState(CONVEYOR_REVERSE);
-    }
-    
-    if (conveyorItemPresent(CONVEYOR_SENSOR_1) &&
-        conveyorGetState() == CONVEYOR_REVERSE) {
-      conveyorSetState(CONVEYOR_OFF);
-    } 
-
-    if (conveyorItemPresent(CONVEYOR_SENSOR_2) &&
-        conveyorGetState() == CONVEYOR_FORWARD) {
-      conveyorSetState(CONVEYOR_OFF);
-    } 
-  } 
 }
+
+static void appTaskBlockPlaced(void *pdata) {
+    canMessage_t rxMsg;
+    canMessage_t txMsg;
+    while(true){
+      if (canReady(CAN_PORT_1)) {    
+
+        canRead(CAN_PORT_1, &rxMsg);       
+         arr = true;
+
+           if(rxMsg.dataA==REQ_PLACEBLOCK){
+//             startWatch();//***********************
+             if (!conveyorItemPresent(CONVEYOR_SENSOR_2)&& arr == true )
+              {
+                txMsg.id = INPUT_ROBOT_ID;
+                txMsg.dataA = ACK_PLACEBLOCK;
+                txMsg.len = 4;
+                canWrite(CAN_PORT_1, &txMsg);
+                arr = false;            
+              } 
+         } 
+     }    
+  }
+  OSTimeDly(20);
+ }
+
 
