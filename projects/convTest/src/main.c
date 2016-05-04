c49783d0c8eacb404c1ad9c5025a89ea8db566b0
jonathan greenlees <jonathan.greenlees@hotmail.co.uk>
2016-05-04 19:12:02 +0000

    main.c edited Added statement to prevent bugs with sensor 2

--- projects/convTest/src/main.c
+++ projects/convTest/src/main.c
@@ -7,7 +7,7 @@
  * Conveyor sensor 2 reported  on interface LED 2 and CONNECT LED
  *
  * item sensed at 1 and moving n reverse -> stop
- * item sensed at 2 and moving 
+ * item sensed at 2 and  
  */
 
 #include <stdbool.h>
